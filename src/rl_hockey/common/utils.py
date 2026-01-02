92ef69ae1997cdb2146483327e3238dab26f4d00
Niklas Abraham GPU <st184432@stud.uni-stuttgart.de>
2026-01-02 10:55:23 +0000

    running simpl etaiing with shooting mode

--- src/rl_hockey/common/utils.py
+++ src/rl_hockey/common/utils.py
@@ -21,3 +21,29 @@ def mirror_action(action):
         mirrored_action[idx] *= -1
 
     return mirrored_action
+
+
+def mirror_discrete_action(discrete_action):
+    """
+    Discrete actions:
+    0: Do nothing
+    1: Move left (x-)
+    2: Move right (x+)
+    3: Move down (y-)
+    4: Move up (y+)
+    5: Rotate CCW
+    6: Rotate CW
+    7: Shoot
+    """
+    mirror_map = {
+        0: 0,  # nothing -> nothing
+        1: 1,  # left -> left
+        2: 2,  # right -> right
+        3: 4,  # down -> up
+        4: 3,  # up -> down
+        5: 6,  # CCW -> CW
+        6: 5,  # CW -> CCW
+        7: 7,  # shoot -> shoot
+    }
+    return mirror_map[discrete_action]
+
