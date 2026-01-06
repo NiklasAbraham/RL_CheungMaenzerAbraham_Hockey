d8e4047e0174505b0763264a26659be1c7bc6db4
anselcheung <anselcheung@yahoo.com.sg>
2026-01-06 15:48:12 +0100

    Add PER to TD3 agent

--- src/rl_hockey/td3/td3.py
+++ src/rl_hockey/td3/td3.py
@@ -6,6 +6,7 @@ from torch.optim.lr_scheduler import LambdaLR
 
 from rl_hockey.common import *
 from rl_hockey.td3.models import Actor, TwinCritic
+from rl_hockey.common.prioritizedbuffer import PERMemory
 
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -37,6 +38,7 @@ class TD3(Agent):
             "policy_noise": 0.2,
             "noise_clip": 0.5,
             "target_network_update_steps": 500,
+            "prioritized_replay": False,
             "verbose": False,
         }
         self.config.update(user_config)
@@ -59,6 +61,15 @@ class TD3(Agent):
         self.noise_clip = self.config["noise_clip"]
         self.target_network_update_steps = self.config["target_network_update_steps"]
         self.verbose = self.config["verbose"]
+        self.prioritized_replay = self.config["prioritized_replay"]
+
+        if self.prioritized_replay:
+            self.buffer = PERMemory(capacity=1000000)
+
+        # Buffer normalization
+        self.transition_mean = np.zeros(state_dim)
+        self.transition_M2 = np.zeros(state_dim)
+        self.transition_count = 0
 
         self.actor = Actor(
             state_dim, action_dim, self.actor_dim, self.actor_n_layers
@@ -105,18 +116,65 @@ class TD3(Agent):
             case _:
                 raise ValueError(f"Unimplemented noise type: {self.config['noise']}")
 
-    def act(self, state):
-        with torch.no_grad():
-            state = torch.from_numpy(state).unsqueeze(0).to(DEVICE)
-            noise = self.exploration_noise_dist.sample((len(state),)).to(DEVICE)
-            pred_action = self.actor(state)
-            new_action = pred_action + noise
-            new_action = torch.clamp(new_action, self.action_min, self.action_max)
+    def normalize_state(self, state: np.ndarray) -> np.ndarray:
+        std = (
+            np.sqrt(self.transition_M2 / self.transition_count)
+            if self.transition_count > 1
+            else np.ones_like(self.transition_mean)
+        )
 
-            return new_action.squeeze(0).cpu().numpy()
+        # Normalize state and next_state
+        normalized_state = (state - self.transition_mean) / (std + 1e-8)
+        return normalized_state
+
+    def update_transition_stats(self, state, next_state, done):
+        self.transition_count += 1
+
+        # Update running mean and variance using Welford's algorithm
+        delta = state - self.transition_mean
+        self.transition_mean += delta / self.transition_count
+        self.transition_M2 += delta * (state - self.transition_mean)
+
+        # Update using next state as well
+        if not done:
+            self.transition_count += 1
+            delta = next_state - self.transition_mean
+            self.transition_mean += delta / self.transition_count
+            self.transition_M2 += delta * (next_state - self.transition_mean)
+
+    def store_transition(self, transition):
+        # Normalize transition before storing
+        state, action, reward, next_state, done = transition
+        self.update_transition_stats(state, next_state, done)
+
+        normalized_state = self.normalize_state(state)
+        normalized_next_state = self.normalize_state(next_state)
+        normalized_transition = (
+            normalized_state,
+            action,
+            reward,
+            normalized_next_state,
+            done,
+        )
+        return super().store_transition(normalized_transition)
+
+    def act(self, state, deterministic=False):
+        with torch.no_grad():
+            state = self.normalize_state(state)
+            state = torch.from_numpy(state).unsqueeze(0).to(DEVICE)
+            action = self.actor(state)
+
+            if not deterministic:
+                noise = self.exploration_noise_dist.sample((len(state),)).to(DEVICE)
+                action = action + noise
+
+            action = torch.clamp(action, self.action_min, self.action_max)
+
+            return action.squeeze(0).cpu().numpy()
 
     def evaluate(self, state):
         with torch.no_grad():
+            state = self.normalize_state(state)
             state = torch.from_numpy(state).unsqueeze(0).to(DEVICE)
 
             # 1. Get the deterministic action (No sampling, no log_prob)
@@ -135,9 +193,22 @@ class TD3(Agent):
         actor_losses = []
 
         for i in range(steps):
-            state, action, reward, next_state, done = self.buffer.sample(
-                self.batch_size
-            )
+            if self.prioritized_replay:
+                (
+                    (
+                        state,
+                        action,
+                        reward,
+                        next_state,
+                        done,
+                    ),
+                    idxs,
+                    is_weight,
+                ) = self.buffer.sample(self.batch_size)
+            else:
+                state, action, reward, next_state, done = self.buffer.sample(
+                    self.batch_size
+                )
 
             state = torch.from_numpy(state).to(dtype=torch.float32, device=DEVICE)
             action = torch.from_numpy(action).to(dtype=torch.float32, device=DEVICE)
@@ -152,8 +223,11 @@ class TD3(Agent):
                 next_action = self.actor_target(next_state)
 
                 # Double clipping with Gaussian Noise
+                sampled_noise = self.policy_noise_dist.sample((self.batch_size,)).to(
+                    DEVICE
+                )
                 next_action += torch.clamp(
-                    self.policy_noise_dist.sample((self.batch_size,)).to(DEVICE),
+                    sampled_noise,
                     -self.noise_clip,
                     self.noise_clip,
                 )
@@ -166,17 +240,36 @@ class TD3(Agent):
                 target = reward + (1 - done) * self.discount * next_value
 
             q1_online, q2_online = self.twincritic_online(state, action)
-            c1_loss = F.mse_loss(q1_online, target)
-            c2_loss = F.mse_loss(q2_online, target)
-            critic_loss = c1_loss + c2_loss
+
+            if self.prioritized_replay:
+                td_errors = (
+                    (torch.abs(q1_online - target) + torch.abs(q2_online - target))
+                    .detach()
+                    .cpu()
+                    .numpy()
+                    # .flatten()
+                )
+                # print(td_errors.shape)
+                for idx, error in zip(idxs, td_errors):
+                    self.buffer.update(idx, error)
+                is_weight = torch.from_numpy(is_weight).to(
+                    dtype=torch.float32, device=DEVICE
+                )
+                c1_loss = F.mse_loss(q1_online, target, reduction="none")
+                c2_loss = F.mse_loss(q2_online, target, reduction="none")
+                critic_loss = torch.mean(is_weight * (c1_loss + c2_loss))
+            else:
+                c1_loss = F.mse_loss(q1_online, target)
+                c2_loss = F.mse_loss(q2_online, target)
+                critic_loss = c1_loss + c2_loss
 
             self.critic_optimizer.zero_grad()
             critic_loss.backward()
 
             # Clip the gradients
-            torch.nn.utils.clip_grad_norm_(
-                self.twincritic_online.parameters(), max_norm=0.5
-            )
+            # torch.nn.utils.clip_grad_norm_(
+            #     self.twincritic_online.parameters(), max_norm=0.5
+            # )
             self.critic_optimizer.step()
 
             critic_losses.append(critic_loss.item())
@@ -184,19 +277,18 @@ class TD3(Agent):
             self.learn_steps_counter += 1
 
             # Accumulative updates for actor
-            if self.learn_steps_counter % self.update_actor_iter != 0:
-                continue
+            if self.learn_steps_counter % self.update_actor_iter == 0:
 
-            self.actor_optimizer.zero_grad()
-            new_action = self.actor(state)
-            actor_q1_loss, _ = self.twincritic_online(state, new_action)
-            actor_loss = -torch.mean(actor_q1_loss)
-            actor_loss.backward()
-            self.actor_optimizer.step()
+                self.actor_optimizer.zero_grad()
+                new_action = self.actor(state)
+                actor_q1_loss, _ = self.twincritic_online(state, new_action)
+                actor_loss = -torch.mean(actor_q1_loss)
+                actor_loss.backward()
+                self.actor_optimizer.step()
 
-            actor_losses.append(actor_loss.item())
+                actor_losses.append(actor_loss.item())
 
-            if self.learn_steps_counter % self.target_network_update_steps:
+            if self.learn_steps_counter % self.target_network_update_steps == 0:
                 self.update_network_parameters()
 
         return {"critic_loss": critic_losses, "actor_loss": actor_losses}
